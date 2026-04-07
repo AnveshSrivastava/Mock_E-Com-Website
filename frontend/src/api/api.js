8e8cb2815b9da3458276500e6e5bab4a9e96f767
Anvesh Srivastava <anveshsrivastava9@gmail.com>
2026-04-08 01:02:58 +0530

    fix: Backend deployed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = "https://mock-e-com-website.onrender.com" || process.env.REACT_APP_BACKEND_URL;
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`,
   headers: {
